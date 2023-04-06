b4de7567ada52bd18b3ba0b37946058cb06821f4
EmanuelVictorSilva <emanuelvictorsilva768@gmail.com>
2023-04-06 13:04:50 -0300

    Exercício Concluído

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 print("Bem vindo à Loja de Tintas do seu ZÉ")
 metros_quadrados = input("Qual a área em m²?\n")
 metros_quadrados = float(metros_quadrados)
+litros_de_tinta = metros_quadrados / 3
+if litros_de_tinta > 18:
+   qtd_de_latas = int(litros_de_tinta/18) + 1
+   valor_total = qtd_de_latas * 80
+   print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
+if litros_de_tinta <= 18 and metros_quadrados > 0:
+   qtd_de_latas = 1 
+   valor_total = qtd_de_latas * 80
+   print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
+   
 
-# Coloque o código para resolver o problema nessa parte do programa
-
-# As duas variáveis qtd_de_latas e valor_total devem guardar a resposta do problema
-# Troque os zeros pelos valores corretos.
-qtd_de_latas = 0
-valor_total = 0
-
-print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
